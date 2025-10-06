32973a88bd4dfb40a8d0bd5fffc98a46514ab52f
shellikerishukla@gmail.com <shellikerishukla@gmail.com>
2025-10-06 16:51:33 +0530

    save to prime-count

--- evod.py
+++ evod.py
@@ -1,4 +1,17 @@
+import math
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
 nums = list(map(int, input("Enter numbers: ").split()))
 evens = sum(1 for x in nums if x % 2 == 0)
 odds = len(nums) - evens
-print("Even:", evens, "Odd:", odds)
+
+print(f"Even: {evens}, Odd: {odds}")
+for num in nums:
+    print(f"{num} is {'Prime' if is_prime(num) else 'Not Prime'}")
